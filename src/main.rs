4deec37457b6f3cc718d74bd0d67adcb134e09e2
David Garcia <davgarmor2@alum.us.es>
2020-07-28 15:48:19 +0200

    ➕First draft

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-        let name = req.match_info().get("name").unwrap_or("World");
-        format!("Hello {}!", &name)
-}
+use actix_files::Files;
+use actix_web::{App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-        HttpServer::new(|| {
-                    App::new()
-                        .route("/", web::get().to(greet))
-                        .route("/{name}", web::get().to(greet))
-        })
-        .bind("46.101.127.22:8000")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/images", "./static/images").show_files_listing())
+            .service(Files::new("/", "./static/root").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
---
+++ static/root/js/index.js
@@ -0,0 +1,4 @@
+window.addEventListener(
+  "load",
+  () => (document.querySelector(".img-bg").height = window.innerHeight)
+);
