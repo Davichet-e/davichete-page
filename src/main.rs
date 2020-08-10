f19566669416e88eefa484c43c65599c3ed063af
David Garcia <davgarmor2@alum.us.es>
2020-08-11 01:06:04 +0200

    🔨 V0.2
    Added some more content, centered layout..

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use actix_web::{App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let addr = "127.0.0.1:8080";
+    println!("Running on http://{}", &addr);
     HttpServer::new(|| {
         App::new()
             .service(Files::new("/images", "./static/images").show_files_listing())
             .service(Files::new("/", "./static/root").index_file("index.html"))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(addr)?
     .run()
     .await
 }
--- static/root/js/index.js
+++ static/root/js/index.js
@@ -1,4 +1,35 @@
-window.addEventListener(
-  "load",
-  () => (document.querySelector(".img-bg").height = window.innerHeight)
-);
+let firstScrolled = false;
+
+const fun = () => {
+  if (isOnScreen(document.querySelector(".skill1"))) {
+    if (!firstScrolled) {
+      ["100%", "60%", "70%", "59%"].forEach((width, i) =>
+        document
+          .querySelector(".progress-bar-" + (i + 1))
+          .animate([{ width: "0" }, { width }], {
+            duration: 2500 / (i + 2),
+            direction: "normal",
+            fill: "forwards",
+          })
+      );
+      firstScrolled = true;
+    } else {
+      window.removeEventListener("scroll", fun);
+    }
+  }
+};
+
+function isOnScreen(element) {
+  const docViewTop = window.scrollY;
+  const docViewBottom = docViewTop + window.innerHeight;
+
+  const elemTop = element.offsetTop;
+  const elemBottom = elemTop + element.style.height;
+
+  return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
+window.addEventListener("scroll", fun);
+// window.removeEventListener(
+//   "DOMNodeRemoved",
+//   window.removeEventListener("scroll", fun)
+// );
