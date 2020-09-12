05bfc53e7397b0dfa9e024e8ec9fde21224eb113
David García Morillo <davidgmorillop@gmail.com>
2020-09-12 08:14:17 +0000

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use actix_files::Files;
-use actix_web::{client::{Connector, Client}, web, App, HttpRequest, HttpServer, Responder, Error as ActixError};
+use actix_files::{Files, NamedFile};
+use actix_web::{client::{Connector, Client}, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Error as ActixError};
 use openssl::ssl::{SslConnector, SslMethod};
 
-async fn api_rae(req: HttpRequest) -> impl Responder { 
+async fn api_rae(req: HttpRequest) -> impl Responder {
     // Create request builder and send request
     let name = req.match_info().get("word").unwrap_or("World");
     let builder = SslConnector::builder(SslMethod::tls()).unwrap();
@@ -25,20 +25,27 @@ async fn api_rae(req: HttpRequest) -> impl Responder {
             .unwrap();     
     }
     let body = response.body().await.unwrap();
-    let map = rae_rust::search(name, std::str::from_utf8(&body).unwrap()).unwrap();
+    let map = rae_rust::search(name, std::str::from_utf8(&body).expect("Failed to parse body"))
+        .expect("Failed to obtain the dicctionary of meanings");
 
     Ok::<_, ActixError>(web::Json(map))
 }
 
+fn not_found(r: HttpRequest) -> HttpResponse {
+    NamedFile::open("./static/root/html/notfound.html".parse::<std::path::PathBuf>().expect("Failed to parse path")).unwrap().set_status_code("404".parse().unwrap()).into_response(&r).unwrap()
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+println!("{:?}", std::fs::canonicalize(&std::path::PathBuf::from("./")));
     let addr = "127.0.0.1:8080";
     println!("Running on http://{}", &addr);
     HttpServer::new(|| {
         App::new()
             .route("/rae/v1/{word}", web::get().to(api_rae))
-            .service(Files::new("/images", "./static/images").show_files_listing())
+            .service(Files::new("/images", "./static/images"))
             .service(Files::new("/", "./static/root").index_file("index.html"))
+            .default_service(web::get().to(not_found))
     })
     .bind(addr)?
     .run()
--- static/root/js/index.js
+++ static/root/js/index.js
@@ -1,20 +1,58 @@
-let firstScrolled = false;
+const pandasImage = document.querySelector("[alt='Pandas logo']")
+const mediaDark = window.matchMedia('(prefers-color-scheme: dark)');
+const rustLogo = document.querySelector("#path3")
 
-const fun = () => {
-  if (isOnScreen(document.querySelector(".skill1"))) {
-    if (!firstScrolled) {
-      ["100%", "60%", "70%", "59%"].forEach((width, i) =>
-        document
-          .querySelector(".progress-bar-" + (i + 1))
-          .animate([{ width: "0" }, { width }], {
+function changeImages(condition) {
+  if (condition) {
+
+    pandasImage.src =
+      "https://pandas.pydata.org/static/img/pandas_white.svg"
+    rustLogo.style.fill = "whitesmoke"
+  } else {
+
+    pandasImage.src =
+      "https://pandas.pydata.org/static/img/pandas.svg"
+    rustLogo.style.fill = "black"
+  }
+}
+
+/** Check if user is in dark mode and select the according image depending on it. */
+function setImagesDarkMode() {
+  changeImages(mediaDark.matches)
+}
+
+mediaDark.addEventListener('change', event => {
+  changeImages(event.matches)
+});
+
+window.addEventListener("load", setImagesDarkMode, { once: true })
+
+const progressBarContainer = document.querySelectorAll('.progress-bar-container > *')
+const firstAnimated = [];
+
+const skills = [/* Python */ "100%", /* Rust */ "60%", /* Vue */ "90%", /* Pandas */ "63%"]
+
+function animateProgressBar() {
+  for (const [i, progressBarElement] of progressBarContainer.entries()) {
+    if (isOnScreen(progressBarElement)) {
+
+      const { className: progressBarClass } = progressBarElement
+      if (!firstAnimated.includes(progressBarClass)) {
+
+        progressBarElement
+          .animate([{ width: "0" }, { width: skills[i] }], {
             duration: 2500 / (i + 2),
             direction: "normal",
             fill: "forwards",
-          })
-      );
-      firstScrolled = true;
-    } else {
-      window.removeEventListener("scroll", fun);
+          });
+        firstAnimated.push(progressBarClass);
+
+      } else {
+
+        if (firstAnimated.length === progressBarContainer.length)
+          window.removeEventListener("scroll", animateProgressBar);
+
+      }
     }
   }
 };
@@ -28,8 +66,19 @@ function isOnScreen(element) {
 
   return elemBottom <= docViewBottom && elemTop >= docViewTop;
 }
-window.addEventListener("scroll", fun);
-// window.removeEventListener(
-//   "DOMNodeRemoved",
-//   window.removeEventListener("scroll", fun)
-// );
+
+window.addEventListener("scroll", animateProgressBar);
+
+
+/**
+ * @TODO
+ * @param {Event} event 
+ */
+function contact(event) {
+  event.preventDefault()
+  const { 0: nameElement, 1: emailElement, 2: messageElement } = event.target
+  const [{ value: name }, { value: email }, { value: message }] =
+    [nameElement, emailElement, messageElement];
+  console.log(name, email, message);
+  return false;
+}
