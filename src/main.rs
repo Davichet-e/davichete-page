7caa5623e9fc33d4928aa00f99f3fc6fb534ff96
David García Morillo <davidgmorillop@gmail.com>
2021-03-27 16:08:42 +0000

    Simplified to only serve the API

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use actix_files::{Files, NamedFile};
 use actix_web::{
     client::{Client, ClientResponse, Connector},
     dev::{Decompress, Payload},
-    web, App, Error as ActixError, HttpRequest, HttpResponse, HttpServer, Responder,
+    middleware, web, App, Error as ActixError, HttpRequest, HttpServer, Responder,
 };
 use openssl::ssl::{SslConnector, SslMethod};
 use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
@@ -12,13 +11,13 @@ async fn api_rae(req: HttpRequest) -> impl Responder {
     /// https://url.spec.whatwg.org/#fragment-percent-encode-set
     const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
 
-    // Create request builder and send request
-    let name = req.match_info().get("word").unwrap_or("World");
+    let name = req.match_info().get("word").unwrap();
 
     // https://en.wikipedia.org/wiki/Percent-encoding
-    let name: &str = &utf8_percent_encode(name, FRAGMENT).collect::<String>();
+    let name: &str = &utf8_percent_encode(name, FRAGMENT).to_string();
     let ssl_connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
 
+    // Create request builder and send request
     let client = Client::builder()
         .connector(Connector::new().ssl(ssl_connector).finish())
         .finish();
@@ -26,20 +25,21 @@ async fn api_rae(req: HttpRequest) -> impl Responder {
     let mut response = make_request_rae(name, &client).await;
 
     if response.status().as_u16() == 301 {
-        response = client
-            .get(
-                "https://dle.rae.es".to_owned()
-                    + &utf8_percent_encode(
-                        std::str::from_utf8(response.headers().get("location").unwrap().as_bytes())
-                            .unwrap(),
-                        FRAGMENT,
-                    )
-                    .collect::<String>(),
+        response = make_request_rae(
+            &utf8_percent_encode(
+                response
+                    .headers()
+                    .get("location")
+                    .and_then(|location| std::str::from_utf8(location.as_bytes()).ok())
+                    .unwrap(),
+                FRAGMENT,
             )
-            .send() // <- Send request
-            .await // <- Wait for response
-            .unwrap();
+            .to_string()[1..],
+            &client,
+        )
+        .await;
     }
+
     let body = response.body().await.unwrap();
 
     let result_rae = rae_rust::search(std::str::from_utf8(&body).expect("Failed to parse body"));
@@ -55,11 +55,8 @@ async fn api_rae(req: HttpRequest) -> impl Responder {
             map
         }
         Err(rae_rust::WebScrapError::Other(word)) => {
-            let mut response = make_request_rae(
-                &utf8_percent_encode(&word, FRAGMENT).collect::<String>(),
-                &client,
-            )
-            .await;
+            let mut response =
+                make_request_rae(&utf8_percent_encode(&word, FRAGMENT).to_string(), &client).await;
             let body = response.body().await.unwrap();
 
             rae_rust::search(std::str::from_utf8(&body).expect("Failed to parse body")).unwrap()
@@ -85,27 +82,17 @@ async fn make_request_rae(word: &str, client: &Client) -> ClientResponse<Decompr
         .unwrap()
 }
 
-fn not_found(req: HttpRequest) -> HttpResponse {
-    let path: std::path::PathBuf = "./static/root/html/notfound.html"
-        .parse()
-        .expect("Failed to parse path");
-    let namedfile = NamedFile::open(path).expect("Failed to create NamedFile");
-    namedfile
-        .set_status_code("404".parse().unwrap())
-        .into_response(&req)
-        .expect("Failed to convert to response")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let addr = "127.0.0.1:8080";
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::NormalizePath::new(
+                middleware::normalize::TrailingSlash::Trim,
+            ))
             .route("/rae/v1/{word}", web::get().to(api_rae))
-            .service(Files::new("/images", "./static/images"))
-            .service(Files::new("/", "./static/root").index_file("index.html"))
-            .default_service(web::get().to(not_found))
     })
+    // Create request builder and send request
     .bind(addr)?
     .run()
     .await
